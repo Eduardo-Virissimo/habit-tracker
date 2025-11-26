5c186502b149a909fdafbed7da4d141e6a9b9a5a
Eduardo <riuugamer777@gmail.com>
2025-11-25 21:33:56 -0300

    first/ base project

---
+++ src/test/java/io/github/eduardo/habittracker/HabitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eduardo.habittracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
